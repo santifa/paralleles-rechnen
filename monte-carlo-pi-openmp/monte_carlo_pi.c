9831297233f2d858c87b0d1cb69d1424ffeab7bb
santifa <juerges.henrik@gmail.com>
2013-12-07 14:40:06 +0100

    Sequentielles Programm funktioniert

--- monte-carlo-pi-openmp/monte_carlo_pi.c
+++ monte-carlo-pi-openmp/monte_carlo_pi.c
@@ -1,43 +1,77 @@
 /*
  * Monte Carlo method to calculate pi.
+ * defined in openmp
  */
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
+#define RNG_MOD 714025
 #define sqr(a)  a*a
 
-/* calculate pi */
-double pi(double tolerance, unsigned long samples) {
 
-    double x, y, value, error;
-    unsigned long sampled = 0, hit = 0, i;
+typedef struct {
+    double result;
+    unsigned long samples;
+} pi_t;
 
-    do {
-        /* max loops are samples, no error checking everytime we loop */
-        for (i = samples; i; i--, sampled ++) {
-            x = rand() / (RAND_MAX + 1.0);
-            y = rand() / (RAND_MAX + 1.0);
-            if (sqr(x) + sqr(y) < 1) {
-                hit++;
-            }
-        }
 
-        value = (double) hit / sampled;
-        error = value * sqrt(value * (1 - value) / sampled) * 4;
-        value *= 4;
+int pr_random(void){
+    static int state = 0;
 
-        fprintf(stderr, "Pi = %f +/- %5.3e at %ldM samples.\r",
-                value, error, sampled / samples);
-
-        /* if hit is fails rand() is broken */
-    } while (!hit || error > tolerance);
-    return value;
+    return (state = (1366 * state + 150889) % RNG_MOD);
 }
 
-int main() {
-    double tolerance = 2e-6;
-    unsigned long samples = 1000000;
-    printf("Pi is %f\n ,with a tolerance of %f.", pi(tolerance, samples), tolerance);
+/*
+ * returns random value between range and range+1
+ */
+float random_range(double range) {
+    return ((double) pr_random() / (double) RNG_MOD) * range;
+}
+
+pi_t *calculate_pi(pi_t *pi) {
+    unsigned long hits = 0;
+    double x, y;
+    int i;
+
+    // berechnung
+    for (i = pi->samples; i; i--) {
+        x = random_range(1.000);
+        y = random_range(1.000);
+        if (sqr(x) + sqr(y) < 1) {
+            hits++;
+        }
+    }
+
+    printf("Hits: %lu \t", hits);
+    pi->result = (double)  4 * hits  / pi->samples;
+    return pi;
+}
+
+int main(int argc, const char *argv[]) {
+    // speicher anfordern
+    pi_t *pi;
+    pi = (pi_t *) malloc(sizeof(pi_t));
+
+    if (argc == 1) {
+        printf("Falsche argument anzahl.\n");
+        free(pi);
+        pi = NULL;
+        exit(EXIT_FAILURE);
+    }
+
+    if (pi == NULL) {
+        printf("Something went wrong.\n");
+        exit(EXIT_FAILURE);
+    } else {
+        // eingabe lesen
+        pi->samples = atoi(argv[1]);
+
+        pi = calculate_pi(pi);
+        printf("Pi ist %f .\n", pi->result);
+        // speicher freigeben
+        free(pi);
+        pi = NULL;
+    }
     return 0;
 }
--- monte-carlo-pi-pthreads/pi.c
+++ monte-carlo-pi-pthreads/pi.c
@@ -6,9 +6,9 @@
 #include <math.h>
 #define M_PI 3.14159265358979323846
 
-typedef struct 
+typedef struct
 {
-	unsigned long iter, value;
+    unsigned long iter, value;
 
 }data;
 
@@ -19,67 +19,67 @@ int pr_random(void){
 }
 
 double pr_random_f(double range){
-	return ((double) pr_random() / (double) RNG_MOD) * range;
+    return ((double) pr_random() / (double) RNG_MOD) * range;
 }
 
 static void *piGenerator(void *arg){
-	data *f=(data *)arg;
-	unsigned long i ;
+    data *f=(data *)arg;
+    unsigned long i ;
 
-	for ( i = 0; i < f->iter; ++i){
-		
-		double x = pr_random_f(1.000);
-		double y = pr_random_f(1.000);
-		
-		
-		if (x*x + y*y <= 1.0){
-			f->value++;
-		}
-	}
-	return arg;
+    for ( i = 0; i < f->iter; ++i){
+
+        double x = pr_random_f(1.000);
+        double y = pr_random_f(1.000);
+
+
+        if (x*x + y*y <= 1.0){
+            f->value++;
+        }
+    }
+    return arg;
 }
 
-int main(int argc, const char *argv[]){	
-	unsigned long thcount = atoi(argv[1]); 
-	unsigned long stepcount = atoi(argv[2]);
-	unsigned long i;
-	pthread_t th[thcount];
-	data *ret[thcount];
-	double erg = 0.0;
-	if (argc != 3){
-		printf("%s\n", "Falsche Parameter anzahl");
-	}
-	else {
+int main(int argc, const char *argv[]){
+    unsigned long thcount = atoi(argv[1]);
+    unsigned long stepcount = atoi(argv[2]);
+    unsigned long i;
+    pthread_t th[thcount];
+    data *ret[thcount];
+    double erg = 0.0;
+    if (argc != 3){
+        printf("%s\n", "Falsche Parameter anzahl");
+    }
+    else {
 
-   		for  ( i = 0; i < thcount; i++) {
+        for  ( i = 0; i < thcount; i++) {
 
-      		ret[i] = (data*)malloc(sizeof(data));
-      		if(ret[i] == NULL) {
-         		printf("Konnte keinen Speicher reservieren ...!\n");
-         		exit(EXIT_FAILURE);
-      		}
-      		
+            ret[i] = (data*)malloc(sizeof(data));
+            if(ret[i] == NULL) {
+                printf("Konnte keinen Speicher reservieren ...!\n");
+                exit(EXIT_FAILURE);
+            }
 
-      		ret[i]->iter = stepcount / thcount;
-      		if (i == 0){
-      			ret[i]->iter = ret[i]->iter + (stepcount % thcount);
-      		}
 
-      		if(pthread_create(&th[i], NULL, &piGenerator, ret[i]) != 0) {
-         		fprintf (stderr, "Konnte Thread nicht erzeugen\n");
-         		exit (EXIT_FAILURE);
-      		}
-   		}
-   		
-		
-		for( i=0; i < thcount; i++){
-      		pthread_join(th[i], (void**) &ret[i]);
-   			erg = erg + ret[i]->value;
-   		}
-   		
-		erg = 4*(erg/stepcount);
-   		printf("Ergebnis: %f\n", erg);
-   		printf("relativer Fehler : %f\n", (erg - M_PI) / M_PI);
-	}
-	return 0;
+            ret[i]->iter = stepcount / thcount;
+            if (i == 0){
+                ret[i]->iter = ret[i]->iter + (stepcount % thcount);
+            }
+
+            if(pthread_create(&th[i], NULL, &piGenerator, ret[i]) != 0) {
+                fprintf (stderr, "Konnte Thread nicht erzeugen\n");
+                exit (EXIT_FAILURE);
+            }
+        }
+
+
+        for( i=0; i < thcount; i++){
+            pthread_join(th[i], (void**) &ret[i]);
+            erg = erg + ret[i]->value;
+        }
+
+        erg = 4*(erg/stepcount);
+        printf("Ergebnis: %f\n", erg);
+        printf("relativer Fehler : %f\n", (erg - M_PI) / M_PI);
+    }
+    return 0;
 }
